ded2add904affa8940626f65f4e27e1749aa3cb8
Edwin Chi <chipineiro@gmail.com>
2020-03-01 17:47:27 -0600

    Episode 8 Passing data from Controller to View Passing data through properties file log.info now working

--- src/main/java/com/example/SpringPractices/TestingController.java
+++ src/main/java/com/example/SpringPractices/TestingController.java
@@ -1,6 +1,9 @@
 package com.example.SpringPractices;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,10 +17,18 @@ public class TestingController {
 }
 
 @Controller
+@Slf4j
 class ControllerTest {
 
+    @Value("${index.propMessage}")
+    private String propMessage;
+
     @GetMapping("/test")
-    public static String test() {
+    public String test(Model model) {
+        String message = "This is a message to display in html file";
+        model.addAttribute("message", message);
+        model.addAttribute("propMessage", propMessage);
+        log.info("Something...");
         return "index";
     }
 
