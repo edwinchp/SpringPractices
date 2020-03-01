79b5f9d65d6c6ee50749c5731cd033db88ded397
Edwin Chi <chipineiro@gmail.com>
2020-03-01 12:48:21 -0600

    fist commit

---
+++ src/main/java/com/example/SpringPractices/SpringPracticesApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringPractices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPracticesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPracticesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringPractices/SpringPracticesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringPractices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPracticesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
