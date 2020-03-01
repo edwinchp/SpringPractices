3e5959a04b99e47a539cdf5b8791e67b40f40f7a
Edwin Chi <chipineiro@gmail.com>
2020-03-01 12:51:51 -0600

    GetMapping Hello World!

--- src/main/java/com/example/SpringPractices/TestingController.java
+++ src/main/java/com/example/SpringPractices/TestingController.java
@@ -1,9 +1,14 @@
 package com.example.SpringPractices;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestingController {
 
+    @GetMapping("/")
+    public static String home(){
+        return "Hello World!";
+    }
 
 }
